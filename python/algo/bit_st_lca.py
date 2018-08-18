75cdb4eb54d0ef6c07ab7cd18a9c0ba6a9074e3c
pigbreeder <35560882+pigbreeder@users.noreply.github.com>
2018-08-18 21:13:53 +0800

    Create bit_st_lca.py

---
+++ python/algo/bit_st_lca.py
@@ -0,0 +1,71 @@
+#encoding=utf8
+import numpy as np
+
+# 树状数组/st/lca
+
+# BinaryIndexTree
+LEN = 10
+bitArr = [0] * LEN
+def BitAdd(idx, x):
+    while idx < LEN:
+        bitArr[idx] += x
+        idx += idx&(-idx)
+
+def BitSum(idx):
+    ret = 0
+    while idx > 0:
+        ret += bitArr[idx]
+        idx -= idx&(-idx)
+    return ret
+
+
+#
+arr = [1,2,10,4444,5]
+for i, a in enumerate(arr, 1):
+    BitAdd(i, a)
+    print i,a,bitArr
+print(BitSum(4))
+
+
+#####################################################
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.child = []
+
+vis = [0] * LEN
+fa = [i for i in range(LEN)]
+nodes = []
+query = []
+
+def find(x):
+    if x == fa[x]:
+        return x
+    else:
+        fa[x] = Find(fa[x])
+        return fa[x]
+
+def LCA(u):
+    for v in nodes[u].child:
+            LCA(v)
+            fa[v] = u
+    
+    vis[u] = 1
+    for v in que[u].child:
+        if vis[v]:
+            ans[u][v] = find(v)
+
+mn = [[0] for i in range(LEN)]
+log2 = [0] * LEN
+def st():
+    for i in range(LEN):
+        log2[i] = -1 if i == 0 else log2[i >> 1] + 1
+    for j in range(1, 20):
+        for i in range(1, LEN+1):
+            if (1<<j) > LEN:
+                break
+            mn[i][j] = min(mn[i][j - 1], mn[i + (1 << j - 1)][j - 1])
+
+def st_query(ql, qr):
+    k = log2[qr - ql + 1];
+    return min(mn[ql][k], mn[qr - (1 << k) + 1][k])
